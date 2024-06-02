6530dcca98ca231cce3ec7999abad85b3913c21e
isahaq1 <hmisahaq01@gmail.com>
2024-06-02 16:40:27 +0600

    sdfsdf

--- server/app.js
+++ server/app.js
@@ -5,8 +5,8 @@ app.get('/', (req, res) => {
 });
 
 app.listen(
-  9000,
+  7000,
   console.log(
-    `CICD Project server running  on port 9000 test`
+    `CICD Project server running  on port 7000 test`
   )
 );
