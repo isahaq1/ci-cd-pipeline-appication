50a89524b061716dc264996829e12a9012886e65
isahaq1 <hmisahaq01@gmail.com>
2024-06-04 14:44:49 +0600

    sdfsdf

--- server/app.js
+++ server/app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World! ddd')
 })
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
