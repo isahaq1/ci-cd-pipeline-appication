0cceb0ed1a53db468885fb57a6808f19058fecc1
isahaq1 <hmisahaq01@gmail.com>
2024-06-04 11:15:48 +0600

    sdf

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 const app = express();
-const port = 32000
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
