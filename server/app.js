903d679576a7ed5921ab06857ee08eeb5d677040
isahaq1 <hmisahaq01@gmail.com>
2024-06-24 14:23:39 +0600

    sdfsd

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Example app Running test Hello DX Numatrix')
+  res.send('Example app Running test Hello DX Numatrix bb')
 })
 
 app.listen(port, () => {
