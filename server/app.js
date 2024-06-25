800852f258e7725ac46e57fb4d981571581f0d4a
isahaq1 <hmisahaq01@gmail.com>
2024-06-25 17:11:48 +0600

    sdfsdf

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import morgan from 'morgan';
 const app = express();
 const port = 3000
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.send('Example app Running test Hello DX Numatrix Ragib')
